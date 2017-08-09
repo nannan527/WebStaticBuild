04a874e0e5ba54c601e6fcdffed7c0413c987b88
orlando <orlando@orlandodeMacBook-Pro.local>
2017-08-09 16:33:03 +0800

    提交git

--- src/content/js/browser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Dongyinan on 2016/2/2.28
- */
-(function browserLayer(){
-    if(navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[^0-9]/ig,"") <= 80){
-        window.location.href='../html/browser.html';
-    };
-})();
