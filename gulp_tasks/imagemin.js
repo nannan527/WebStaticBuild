0336e6e956d65d59d83969cd09adc88ff8cd03dc
Orlando <dongyinan@syncsoftinc.com>
2018-02-08 16:37:20 +0800

    【livnco2018】项目上传

--- gulp_tasks/imagemin.js
+++ gulp_tasks/imagemin.js
@@ -1,51 +1,62 @@
 var gulp = require('gulp'),
-    debug = require('gulp-debug'),
-    plumber = require('gulp-plumber'),
-    cached = require('gulp-cached'),
-    changed = require('gulp-changed'),
-    gulpif = require('gulp-if'),
-    imagemin = require('gulp-imagemin'),
-    img_flag = 0;
+  debug = require('gulp-debug'),
+  plumber = require('gulp-plumber'),
+  cached = require('gulp-cached'),
+  changed = require('gulp-changed'),
+  gulpif = require('gulp-if'),
+  imagemin = require('gulp-imagemin'),
+  img_flag = 0;
 
 // 加载路径配置文件
 var config = require('../gulp_config.json');
 
 //图片压缩
 gulp.task('imagemin', function() {
-    return gulp.src(config.src.imgPath)
-        .pipe(plumber())
-        .pipe(changed(config.dist.imgPath))
-        .pipe(cached('png,jpg,gif,svg'))
-        .pipe(imagemin([
-            imagemin.gifsicle({
-                interlaced: true
-            }),
-            imagemin.jpegtran({
-                progressive: true
-            }),
-            imagemin.optipng(),
-            imagemin.svgo({
-                plugins: [{
-                    removeUselessDefs: false
-                }, {
-                    cleanupIDs: false
-                }, {
-                    removeViewBox: false
-                }, {
-                    convertPathData: false
-                }, {
-                    mergePaths: false
-                }, {
-                    removeXMLProcInst: false
-                }, {
-                    removeUnknownsAndDefaults: false
-                }]
-            })
-
-        ], {
-            verbose: true
-        }))
-        .pipe(gulp.dest(config.dist.imgPath));
-
-
+  return gulp
+    .src([config.src.imgPath])
+    .pipe(plumber())
+    .pipe(changed(config.dist.imgPath))
+    .pipe(cached('png,jpg,gif,svg'))
+    .pipe(
+      imagemin(
+        [
+          imagemin.gifsicle({
+            interlaced: true
+          }),
+          imagemin.jpegtran({
+            progressive: true
+          }),
+          imagemin.optipng(),
+          imagemin.svgo({
+            plugins: [
+              {
+                removeUselessDefs: false
+              },
+              {
+                cleanupIDs: false
+              },
+              {
+                removeViewBox: false
+              },
+              {
+                convertPathData: false
+              },
+              {
+                mergePaths: false
+              },
+              {
+                removeXMLProcInst: false
+              },
+              {
+                removeUnknownsAndDefaults: false
+              }
+            ]
+          })
+        ],
+        {
+          verbose: true
+        }
+      )
+    )
+    .pipe(gulp.dest(config.dist.imgPath));
 });
--- gulp_tasks/js.js
+++ gulp_tasks/js.js
@@ -1,23 +1,27 @@
 var gulp = require('gulp'),
-    debug = require('gulp-debug'),
-    babel = require('gulp-babel'),
-    plumber = require('gulp-plumber'),
-    changed = require('gulp-changed');
+  debug = require('gulp-debug'),
+  babel = require('gulp-babel'),
+  plumber = require('gulp-plumber'),
+  changed = require('gulp-changed');
 
 // 加载路径配置文件
 var config = require('../gulp_config.json');
 
-gulp.task('js', function(){
-       gulp.src([config.src.jsPath,'!' + config.src.jsvendorPath])
-        .pipe(plumber())
-        .pipe(changed(config.dist.jsPath))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest(config.dist.jsPath));
+gulp.task('js', function() {
+  gulp
+    .src([config.src.jsPath, '!' + config.src.jsvendorPath])
+    .pipe(plumber())
+    .pipe(changed(config.dist.jsPath))
+    .pipe(
+      babel({
+        presets: ['es2015']
+      })
+    )
+    .pipe(gulp.dest(config.dist.jsPath));
 
-       gulp.src(config.src.jsvendorPath)
-        .pipe(plumber())
-        .pipe(changed(config.dist.jsvendorPath))
-        .pipe(gulp.dest(config.dist.jsvendorPath));
+  gulp
+    .src(config.src.jsvendorPath)
+    .pipe(plumber())
+    .pipe(changed(config.dist.jsvendorPath))
+    .pipe(gulp.dest(config.dist.jsvendorPath));
 });
